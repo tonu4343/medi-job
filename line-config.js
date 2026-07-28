0e454e66f976c99d1b7370b09eae1e8dcfe66892
tonu4343 <tonusree555@gmail.com>
2026-07-28 15:34:04 +0900

    Set the real LINE Official Account add-friend link for push notifications

--- line-config.js
+++ line-config.js
@@ -5,5 +5,5 @@ window.MEDISPOT_LINE = {
   // Account used for push notifications (not the Login channel above).
   // Find it on that channel's Basic settings page as "Bot basic ID" or
   // "QR code" - the URL form is https://line.me/R/ti/p/@your-basic-id
-  officialAccountAddFriendUrl: "YOUR_LINE_OA_ADD_FRIEND_URL"
+  officialAccountAddFriendUrl: "https://line.me/R/ti/p/@089txouf"
 };
